ef4ff6dea73cb8fe08a88bea1407c66ddc45698b
J.D. Bean <jonathandbean@gmail.com>
2018-05-31 14:29:55 -0400

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,39 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state = { pets: [] }, action) {
+  switch (action.type) {
+
+    case 'ADD_PET':
+      return {...state, pets: [...state.pets, {name: action.pet.name, species: action.pet.species, id: action.pet.id }]}
+
+    case 'REMOVE_PET':
+      const petIndex = state.pets.findIndex((pet) => pet.id === action.id)
+      const newPets = [...state.pets]
+      newPets.splice(petIndex, 1)
+      return {...state, pets: newPets}
+
+
+
+    default:
+      return state;
+  }
 }
 
-export function dispatch(){
+
+export function dispatch(action){
+  state = managePets(state, action)
+  render()
 }
 
 export function render(){
+  // const PetsRender = state.pets.map((pet) => `<li>${pet.name}</li>`).join("\n")
+  const PetsRender = state.pets.map((pet) => `<li>${pet.name}</li>`).join(' ')
+  const Container = document.querySelector("#container")
+  // Container.innerHTML =`
+  //   <ul>
+  //     ${PetsRender}
+  //   </ul>`
+  Container.innerHTML =`<ul>${PetsRender}</ul>`
 
 }
